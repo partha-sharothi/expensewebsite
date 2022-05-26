854783225f4906f4ba7a1fd27b9d905b7899d52a
partha-sharothi <p.sharothi@gmail.com>
2022-05-26 11:31:03 +0600

    customised django admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -3,5 +3,11 @@ from .models import Expense, Category
 
 # Register your models here.
 
-admin.site.register(Expense)
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner','category','date',)
+    search_fields = ('description','category','date',)
+
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
