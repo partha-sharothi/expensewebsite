fe45b792e84cee1b6ed1e97e7140f923bea05850
partha-sharothi <p.sharothi@gmail.com>
2022-05-26 11:45:32 +0600

    i forgot to save expenses/admin.py file

--- expenses/admin.py
+++ expenses/admin.py
@@ -8,6 +8,8 @@ class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('amount', 'description', 'owner','category','date',)
     search_fields = ('description','category','date',)
 
+    list_per_page = 5
+
 
 admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
